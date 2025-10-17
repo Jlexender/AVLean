d15126ce7c89d91d2cc640057a415f1c5317ccbe
Alexander Kaliev <aleksandrdmitryevich@gmail.com>
2025-10-17 03:51:36 +0300

    BinTree, depth, balancedproperty

---
+++ AVLean/Tree.lean
@@ -0,0 +1,17 @@
+
+inductive BinaryTree (α : Type)
+  | leaf : BinaryTree α
+  | node (left : BinaryTree α) (right : BinaryTree α)
+
+
+def depth (tree : BinaryTree α) : Nat :=
+  match tree with
+  | .leaf => 0
+  | .node l r => 1 + max (depth l) (depth r)
+
+def isBalanced (tree : BinaryTree α) : Bool :=
+  match tree with
+  | .leaf => true
+  | .node l r => Int.natAbs (depth l - depth r) <= 1
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AVLean
+
+Источники (sources):
+
+* https://neerc.ifmo.ru/wiki/index.php?title=АВЛ-дерево
