e705cd78df824921ad5a4ca876b300e258e25e0c
Alexander Kaliev <aleksandrdmitryevich@gmail.com>
2025-11-08 10:12:13 +0000

    basic lemmas

--- avltree/Tree.lean
+++ avltree/Tree.lean
@@ -3,9 +3,9 @@ namespace BinaryTree_n
 
 /-- Binary Tree Definition
 
-  X
+  V
  / \
-X   X
+L   R
 
 --/
 inductive BinaryTree where
@@ -23,29 +23,47 @@ def size : BinaryTree → Nat
   | BinaryTree.leaf => 1
   | BinaryTree.node left _ right => 1 + size left + size right
 
-
-/-- Example binary tree.
-
-  2
- / \
-1   3
-
--/
-def exampleTree : BinaryTree :=
-  BinaryTree.node
-    (BinaryTree.node BinaryTree.leaf 1 BinaryTree.leaf)
-    2
-    (BinaryTree.node BinaryTree.leaf 3 BinaryTree.leaf)
-
-#eval depth exampleTree
-#eval size exampleTree
-
-
 @[simp]
 theorem depth_leaf : depth BinaryTree.leaf = 0 := rfl
 
 @[simp]
 theorem size_leaf : size BinaryTree.leaf = 1 := rfl
 
+/--
+Retrieve the left child of a binary tree.
+-/
+def leftChild : BinaryTree → BinaryTree
+  | BinaryTree.leaf => BinaryTree.leaf
+  | BinaryTree.node left _ _ => left
+
+/--
+Retrieve the right child of a binary tree.
+-/
+def rightChild : BinaryTree → BinaryTree
+  | BinaryTree.leaf => BinaryTree.leaf
+  | BinaryTree.node _ _ right => right
+
+/--
+Check if a binary tree is a leaf.
+-/
+def isLeaf : BinaryTree → Bool
+  | BinaryTree.leaf => true
+  | BinaryTree.node _ _ _ => false
+
+theorem size_node_leaf : ∀ node, isLeaf node → size node = 1 := by
+  intro node
+  cases node
+  · intro h
+    exact size_leaf
+  · intro h
+    contradiction
+
+theorem size_node_non_leaf : ∀ node, ¬ isLeaf node → size node = 1 + size (leftChild node) + size (rightChild node) := by
+  intro node
+  cases node
+  · intro h
+    contradiction
+  · intro h
+    exact rfl
 
 end BinaryTree_n
